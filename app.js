c7fb8c27809eb8a599992c2620b0e73b16089d30
Steven Basher <sbasher314@gmail.com>
2021-02-02 12:34:22 -0500

    Created simple GroceryList component

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ let App = () => (
   <div>Hello World~!</div>
 );
 
-ReactDOM.render(<App />, document.getElementById("app"));
+let GroceryList = () => (
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
